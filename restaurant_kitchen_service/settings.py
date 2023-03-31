4f1a26dd28cd2f4698c720e46c41cec55f6027c4
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-03-31 11:57:52 +0300

    Added jazzmin to settings.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -31,16 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "jazzmin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "restaurant",
     "crispy_forms",
     "crispy_bootstrap4",
     "theme_material_kit",
+
 ]
 
 MIDDLEWARE = [
