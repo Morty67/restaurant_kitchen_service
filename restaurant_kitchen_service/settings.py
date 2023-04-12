4c5399d93e8337593b00bc094917c34c15fdf460
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-12 11:41:57 +0300

    change secret key in settings.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-SECRET_KEY = 'django-insecure-bw!uaslwr-_*l(5j5w)ni$z@z9(cv)l%edlf4)fo847+1mde7_'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
